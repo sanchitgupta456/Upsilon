4829daa14e4a2fc436c2f03d0cc88088a2e923d6
VedantS01 <65148655+VedantS01@users.noreply.github.com>
2020-10-08 14:53:02 +0530

    search_courses.xml

---
+++ app/src/main/java/com/sanchit/learnzy/CourseListAdapter.java
@@ -0,0 +1,57 @@
+package com.sanchit.learnzy;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CourseListAdapter extends RecyclerView.Adapter<CourseListAdapter.ViewHolder> {
+
+    private static final String TAG = "CourseListAdapter";
+
+    private ArrayList<String> courseNames = new ArrayList<>();
+    private Context context;
+
+    public CourseListAdapter(Context context, ArrayList<String> courseNames) {
+        this.courseNames = courseNames;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.course_list_item, parent, false);
+        ViewHolder holder = new ViewHolder(view) ;
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: added to screen : " + courseNames.get(position));
+        holder.textView.setText(courseNames.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return courseNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textView;
+        RelativeLayout relativeLayout;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = (TextView)itemView.findViewById(R.id.courseListItemText);
+            relativeLayout = (RelativeLayout)itemView.findViewById(R.id.courseListItem);
+        }
+    }
+}
---
+++ app/src/main/java/com/sanchit/learnzy/SearchActivity.java
@@ -0,0 +1,50 @@
+package com.sanchit.learnzy;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class SearchActivity extends AppCompatActivity {
+    private static final String TAG = "SearchActivity";
+
+    private ArrayList<String> courseNames = new ArrayList<>();
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_courses);
+        Log.d(TAG, "onCreate: started.");
+        initCourseNames();
+
+
+    }
+
+    private void initCourseNames () {
+        Log.d(TAG, "initCourseNames: added names.");
+        courseNames.add("T1") ;
+        courseNames.add("T2") ;
+        courseNames.add("T3") ;
+        courseNames.add("T4") ;
+        courseNames.add("T5") ;
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView () {
+        RecyclerView recyclerView = findViewById(R.id.courseList) ;
+        CourseListAdapter adapter = new CourseListAdapter(this, courseNames);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
