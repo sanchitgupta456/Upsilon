8f86a6f010dec03937b904cbc5e3f1159a86f4be
Sanchit Gupta <sanchitgupta456@gmail.com>
2020-10-29 22:46:01 +0530

    .TEMP

--- app/src/main/java/com/sanchit/Upsilon/AddCourseActivity.java
+++ app/src/main/java/com/sanchit/Upsilon/AddCourseActivity.java
@@ -1,8 +1,13 @@
 package com.sanchit.Upsilon;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,7 +26,10 @@ public class AddCourseActivity extends AppCompatActivity {
 
     String appID = "upsilon-ityvn";
     EditText CourseName,CourseDescription,CourseDuration,NumberOfBatches;
-    RadioButton Online,Offline,Group,Individual,Free,Paid;
+    String courseName,courseDescription,courseDuration,numOfBatches,mode;
+    Button addCourseButton;
+    RadioButton Group,Individual,Free,Paid;
+    ToggleButton offline_online;
     App app;
     User user;
     MongoClient mongoClient;
@@ -36,6 +44,8 @@ public class AddCourseActivity extends AppCompatActivity {
         CourseDescription = (EditText) findViewById(R.id.add_course_description);
         CourseDuration = (EditText) findViewById(R.id.add_course_duration);
         NumberOfBatches = (EditText) findViewById(R.id.add_course_num_batches);
+        addCourseButton = (Button) findViewById(R.id.btnAddCourse);
+        offline_online = (ToggleButton) findViewById(R.id.add_course_mode);
 
         app = new App(new AppConfiguration.Builder(appID)
                 .build());
@@ -43,6 +53,45 @@ public class AddCourseActivity extends AppCompatActivity {
         mongoClient = user.getMongoClient("mongodb-atlas");
         mongoDatabase = mongoClient.getDatabase("Upsilon");
         MongoCollection<Document> mongoCollection  = mongoDatabase.getCollection("CourseData");
+
+        addCourseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                courseName = CourseName.getText().toString();
+                courseDescription = CourseDescription.getText().toString();
+                courseDuration = CourseDuration.getText().toString();
+                numOfBatches = NumberOfBatches.getText().toString();
+                if(offline_online.isChecked())
+                {
+                    mode="Online";
+                }
+                else
+                {
+                    mode="Offline";
+                }
+
+                Document courseDetails = new Document();
+                courseDetails.append("courseName",courseName);
+                /*courseDetails.append("courseDescription",courseDescription);
+                courseDetails.append("courseDuration",courseDuration);
+                courseDetails.append("mode",mode);
+                courseDetails.append("numberOfBatches",numOfBatches);*/
+
+                mongoCollection.insertOne(courseDetails.append("courseId","heha")).getAsync(result -> {
+                    if(result.isSuccess())
+                    {
+                        Toast.makeText(getApplicationContext(),"Successfully Added The Course",Toast.LENGTH_LONG).show();
+                    }
+                    else
+                    {
+                        Log.v("User",result.getError().toString());
+                    }
+                });
+
+
+
+            }
+        });
 
 
 
