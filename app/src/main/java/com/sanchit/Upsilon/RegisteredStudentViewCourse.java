65ea98ea91a55938fa980789382d0bfd2fa922df
VedantS01 <65148655+VedantS01@users.noreply.github.com>
2020-11-24 14:22:41 +0530

    added animation resource files for shaking error text boxes

--- app/src/main/java/com/sanchit/Upsilon/RegisteredStudentViewCourse.java
+++ app/src/main/java/com/sanchit/Upsilon/RegisteredStudentViewCourse.java
@@ -3,6 +3,7 @@ package com.sanchit.Upsilon;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisteredStudentViewCourse extends AppCompatActivity {
@@ -11,6 +12,9 @@ public class RegisteredStudentViewCourse extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_active_course);
-
+        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.action_bar_active_course);
+        getSupportActionBar().setElevation(12);
     }
 }
