7a507a3714892edebd27a1666ffaf428b369e9ef
saurav <sauravkale2019@gmail.com>
2021-01-05 23:13:44 +0530

    blank

--- webpages/js/main.js
+++ webpages/js/main.js
@@ -1,62 +1,20 @@
 
-(function ($) {
-    "use strict";
+var submit = document.getElementById("submitButton");
+var title = document.getElementById("title");
 
-    
-    /*==================================================================
-    [ Validate ]*/
-    var name = $('.validate-input input[name="name"]');
-    var email = $('.validate-input input[name="email"]');
-    var subject = $('.validate-input input[name="subject"]');
-    var message = $('.validate-input textarea[name="message"]');
+submit.addEventListener("click", changePassword);
 
+function changePassword(){
+    console.log("Submit pressed!");
+    //resetPwd();
+    //window.location = "Upsilon://";
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get('token');
+    const tokenId = params.get('tokenId');
 
-        if($(name).val().trim() == ''){
-            showValidate(name);
-            check=false;
-        }
+    console.log(token);
+    console.log(tokenId);
 
-        if($(subject).val().trim() == ''){
-            showValidate(subject);
-            check=false;
-        }
-
-
-        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-            showValidate(email);
-            check=false;
-        }
-
-        if($(message).val().trim() == ''){
-            showValidate(message);
-            check=false;
-        }
-
-        return check;
-    });
-
-
-    $('.validate-form .input1').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-       });
-    });
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
-
-})(jQuery);
+    return false;
+}
