ad8a93afc3644a87b2de59ac0d1b951a4114acf8
Sanchit <sanchitgupta456@gmail.com>
2021-02-04 12:28:46 +0530

    Sorted menus

--- app/src/main/java/com/sanchit/Upsilon/MainActivity.java
+++ app/src/main/java/com/sanchit/Upsilon/MainActivity.java
@@ -779,7 +779,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         else if(id==R.id.homeDrawerMenuItem4)
         {
-            Intent intent = new Intent(MainActivity.this,TeacherInfoActivity.class);
+            Intent intent = new Intent(MainActivity.this,UserDataSetupActivity.class);
             startActivity(intent);
         }
         else if(id==R.id.homeDrawerMenuItem5)
