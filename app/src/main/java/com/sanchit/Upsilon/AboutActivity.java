6a4925ea982d9e599d20198ca9ae526e73fd885a
VedantS01 <65148655+VedantS01@users.noreply.github.com>
2021-03-24 23:19:11 +0530

    Design Fixes

--- app/src/main/java/com/sanchit/Upsilon/AboutActivity.java
+++ app/src/main/java/com/sanchit/Upsilon/AboutActivity.java
@@ -3,12 +3,15 @@ package com.sanchit.Upsilon;
 import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-public class AboutActivity extends Activity {
+
+public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+        getSupportActionBar().hide();
     }
 }
 
