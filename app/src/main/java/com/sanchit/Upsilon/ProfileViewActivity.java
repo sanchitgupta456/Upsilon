8fcab43bf20ae2446401aad9967b2b0a9a667076
VedantS01 <65148655+VedantS01@users.noreply.github.com>
2021-06-26 03:28:56 +0530

    update ProfileViewActivity

--- app/src/main/java/com/sanchit/Upsilon/ProfileViewActivity.java
+++ app/src/main/java/com/sanchit/Upsilon/ProfileViewActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -14,7 +15,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.squareup.picasso.Picasso;
 
 import org.bson.Document;
@@ -33,15 +36,16 @@ import io.realm.mongodb.mongo.iterable.MongoCursor;
 
 public class ProfileViewActivity extends AppCompatActivity {
 
-    TextView UserName,PhoneNumber,Email,NumberOfCoursesTaken,NumberOfCoursesTaught;
+    TextInputEditText UserName,PhoneNumber,Email;
     CircleImageView profileImage;
     App app;
     String appID = "upsilon-ityvn";
+    Button update;
 
     MongoClient mongoClient;
     MongoDatabase mongoDatabase;
     private ProgressBar progressBar;
-    private View bar;
+//    private View bar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,12 +53,13 @@ public class ProfileViewActivity extends AppCompatActivity {
         setContentView(R.layout.user_profile);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.your_profile);
-        bar = getSupportActionBar().getCustomView();
-        UserName = (TextView) findViewById(R.id.profileName);
-        PhoneNumber = (TextView) findViewById(R.id.profilePhone);
-        Email = (TextView) findViewById(R.id.profileEmail);
-        NumberOfCoursesTaken = (TextView) findViewById(R.id.profileNumCoursesTaken);
-        NumberOfCoursesTaught = (TextView) findViewById(R.id.profileNumCoursesTaught);
+//        bar = getSupportActionBar().getCustomView();
+        UserName = (TextInputEditText) findViewById(R.id.editTextUserName);
+        PhoneNumber = (TextInputEditText) findViewById(R.id.editTextPhoneNumber);
+        Email = (TextInputEditText) findViewById(R.id.editTextEmailId);
+        update = (Button) findViewById(R.id.updateChange);
+//        NumberOfCoursesTaken = (TextView) findViewById(R.id.profileNumCoursesTaken);
+//        NumberOfCoursesTaught = (TextView) findViewById(R.id.profileNumCoursesTaught);
         profileImage = (CircleImageView) findViewById(R.id.imgProfileImage);
         progressBar = (ProgressBar) findViewById(R.id.loadingUserProfile);
         app = new App(new AppConfiguration.Builder(appID)
@@ -121,21 +126,29 @@ public class ProfileViewActivity extends AppCompatActivity {
             progressBar.setVisibility(View.GONE);
         });
 
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: update fields in backend
+            }
+        });
+
+
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //menu.add(0);
         //menu.getItem(0).setIcon(getDrawable(R.drawable.edit_icon_light));
         menu.add("Edit");
-        menu.getItem(0).setIcon(getDrawable(R.drawable.edit_icon));
+        menu.getItem(0).setIcon(AppCompatResources.getDrawable(getApplicationContext(), R.drawable.edit_icon));
         menu.getItem(0).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         return true;
-    }
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getActionView()==bar){
+        if(item.getItemId()==android.R.id.home){
             finish();
         }
         return super.onOptionsItemSelected(item);
