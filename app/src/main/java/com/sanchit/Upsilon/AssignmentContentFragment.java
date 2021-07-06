7428898758a4c4a2777c79710f5fe402c2fadd7e
VedantS01 <65148655+VedantS01@users.noreply.github.com>
2021-07-06 16:38:31 +0530

    design updates v5.1.1

--- app/src/main/java/com/sanchit/Upsilon/AssignmentContentFragment.java
+++ app/src/main/java/com/sanchit/Upsilon/AssignmentContentFragment.java
@@ -9,8 +9,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
 import com.sanchit.Upsilon.ui.assignment.AssignmentStudentData;
 
 import java.io.IOException;
@@ -33,6 +35,8 @@ public class AssignmentContentFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_assignment_content, container, false);
         String url = "https://res.cloudinary.com/upsilon175/image/upload/v1625568680/Upsilon/Courses/5facf7889f30789191b2afea/assign3_eavpwl.pdf";
         PDFView pdfView = view.findViewById(R.id.contentView);
+        FrameLayout loading = view.findViewById(R.id.loading);
+        loading.setVisibility(View.VISIBLE);
 
         Thread thread = new Thread(new Runnable() {
 
@@ -42,7 +46,12 @@ public class AssignmentContentFragment extends Fragment {
                     InputStream input = null;
                     try {
                         input = new URL(url).openStream();
-                        pdfView.fromStream(input).load();
+                        pdfView.fromStream(input).onLoad(new OnLoadCompleteListener() {
+                            @Override
+                            public void loadComplete(int nbPages) {
+                                loading.setVisibility(View.GONE);
+                            }
+                        }).load();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }                } catch (Exception e) {
