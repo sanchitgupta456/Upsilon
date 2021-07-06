295a7d96e8c6b1320eb7ca87462323d3817a139c
VedantS01 <65148655+VedantS01@users.noreply.github.com>
2021-07-06 15:43:32 +0530

    update assignments v3.0.2

--- app/src/main/java/com/sanchit/Upsilon/AssignmentContentFragment.java
+++ app/src/main/java/com/sanchit/Upsilon/AssignmentContentFragment.java
@@ -3,12 +3,15 @@ package com.sanchit.Upsilon;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.github.barteksc.pdfviewer.PDFView;
+import com.sanchit.Upsilon.ui.assignment.AssignmentStudentData;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,6 +19,7 @@ import java.net.URL;
 
 
 public class AssignmentContentFragment extends Fragment {
+    AssignmentStudentData assignmentStudentData;
 
 
     public AssignmentContentFragment() {
@@ -35,6 +39,9 @@ public class AssignmentContentFragment extends Fragment {
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
+
+        assert getArguments() != null;
+        assignmentStudentData = (AssignmentStudentData) getArguments().getSerializable("Assignment");
         return view;
     }
 }
